f13e5e13839e991d892b96fc646381544d59cc29
Erick Almeida <erickingles@gmail.com>
2022-02-23 22:45:08 -0300

    Implementa o algoritmo por completo

--- busca-heuristica/js/main.js
+++ busca-heuristica/js/main.js
@@ -1,5 +1,14 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { MinHeap } from './min-heap.js';
-import { TRAIN_SPEED_IN_KILOMETERS_PER_HOUR, LINE_EXCHANGE_TIME_IN_MINUTES, STATIONS, } from './data.js';
+import { TRAIN_SPEED_IN_KILOMETERS_PER_HOUR, STATIONS, } from './data.js';
 (() => {
     const departureStationSelectionDropdown = document.getElementById('departure-station-selection-dropdown');
     const destinationStationSelectionDropdown = document.getElementById('destination-station-selection-dropdown');
@@ -7,23 +16,32 @@ import { TRAIN_SPEED_IN_KILOMETERS_PER_HOUR, LINE_EXCHANGE_TIME_IN_MINUTES, STAT
     let selectedDepartureStation = null;
     let selectedDestinationStation = null;
     const updateCalculateRouteButtonAvailability = () => {
-        if (selectedDepartureStation != null && selectedDestinationStation != null) {
+        if (selectedDepartureStation != null && selectedDestinationStation != null && selectedDepartureStation !== selectedDestinationStation) {
             calculateRouteButton.disabled = false;
         }
         else {
             calculateRouteButton.disabled = true;
         }
     };
+    const clearInterface = () => {
+        changeMapOpacity('show');
+        document.getElementById('route-time-indicator').classList.add('hidden-element');
+    };
     departureStationSelectionDropdown.addEventListener('sl-change', (event) => {
         selectedDepartureStation = event.target.value;
         updateCalculateRouteButtonAvailability();
+        clearInterface();
     });
     destinationStationSelectionDropdown.addEventListener('sl-change', (event) => {
         selectedDestinationStation = event.target.value;
         updateCalculateRouteButtonAvailability();
+        clearInterface();
     });
     calculateRouteButton.addEventListener('click', () => {
         const aStarResult = runAStarAlgorithm(selectedDepartureStation, selectedDestinationStation);
+        const path = processAStarPath(aStarResult, selectedDepartureStation, selectedDestinationStation);
+        const tripTime = aStarResult.get(selectedDestinationStation).realTimeFromDepartureToNodeInMinutes;
+        updateInterfaceWithRoute(path, tripTime);
     });
 })();
 function runAStarAlgorithm(departureStation, destinationStation) {
@@ -37,28 +55,27 @@ function runAStarAlgorithm(departureStation, destinationStation) {
         estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(departureStation, destinationStation)
     });
     const closedList = new Map();
-    let isFinalDestination = false;
-    while (openList.size > 0 && !isFinalDestination) {
-        const currentNode = openList.removeSmallest();
+    while (openList.size > 0) {
+        let currentNode = openList.removeSmallest();
         if (currentNode.station === destinationStation) {
-            isFinalDestination = true;
+            return closedList;
         }
-        else {
-            const adjacentStations = STATIONS[currentNode.station].realDistances;
-            for (const [adjacentStation, realDistance] of Object.entries(adjacentStations)) {
-                if (realDistance != null) {
-                    const realTimeFromDepartureToNodeInMinutes = currentNode.realTimeFromDepartureToNodeInMinutes +
-                        getRealTripTimeInMinutes(currentNode.station, adjacentStation, null); // FIXME
-                    if (!closedList.has(adjacentStation) || realTimeFromDepartureToNodeInMinutes < closedList.get(adjacentStation).realTimeFromDepartureToNodeInMinutes) {
-                        closedList.set(currentNode.station, {
-                            station: adjacentStation,
-                            realTimeFromDepartureToNodeInMinutes,
-                            estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation)
-                        });
+        const adjacentStations = STATIONS[currentNode.station].realDistances;
+        for (const [adjacentStation, realDistance] of Object.entries(adjacentStations)) {
+            if (realDistance != null) {
+                const realTimeFromDepartureToNodeInMinutes = currentNode.realTimeFromDepartureToNodeInMinutes +
+                    getRealTripTimeInMinutes(currentNode.station, adjacentStation);
+                if (!closedList.has(adjacentStation) || realTimeFromDepartureToNodeInMinutes < closedList.get(adjacentStation).realTimeFromDepartureToNodeInMinutes) {
+                    closedList.set(adjacentStation, {
+                        station: currentNode.station,
+                        realTimeFromDepartureToNodeInMinutes,
+                        estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation),
+                    });
+                    if (!openList.find(node => (node === null || node === void 0 ? void 0 : node.station) === adjacentStation)) {
                         openList.insert({
                             station: adjacentStation,
                             realTimeFromDepartureToNodeInMinutes,
-                            estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation)
+                            estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation),
                         });
                     }
                 }
@@ -66,18 +83,35 @@ function runAStarAlgorithm(departureStation, destinationStation) {
         }
     }
 }
+function processAStarPath(aStarResult, departureStation, destinationStation) {
+    const path = [destinationStation];
+    while (path[0] !== departureStation) {
+        path.unshift(aStarResult.get(path[0]).station);
+    }
+    return path;
+}
 function calculateTrainTimeInMinutes(distance) {
     return distance / TRAIN_SPEED_IN_KILOMETERS_PER_HOUR * 60;
 }
 function getEstimatedTripTimeInMinutes(departureStation, destinationStation) {
     return calculateTrainTimeInMinutes(STATIONS[departureStation].estimatedDistances[destinationStation]);
 }
-function getRealTripTimeInMinutes(departureStation, destinationStation, currentLine) {
-    let tripTime = calculateTrainTimeInMinutes(STATIONS[departureStation].realDistances[destinationStation]);
-    if (!STATIONS[destinationStation].lines.includes(currentLine)) {
-        tripTime += LINE_EXCHANGE_TIME_IN_MINUTES;
-    }
-    return tripTime;
+function getRealTripTimeInMinutes(departureStation, destinationStation) {
+    return calculateTrainTimeInMinutes(STATIONS[departureStation].realDistances[destinationStation]);
+}
+function updateInterfaceWithRoute(path, tripTime) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const routeTimeIndicator = document.getElementById('route-time-indicator');
+        routeTimeIndicator.innerHTML = `Tempo da rota: <strong>${tripTime.toLocaleString('pt-BR', { maximumFractionDigits: 2 })} minutos</strong>`;
+        routeTimeIndicator.classList.remove('hidden-element');
+        changeMapOpacity('fade');
+        for (let i = 0; i + 1 < path.length; i++) {
+            changeStationOpacity(path[i], 'show');
+            changeLineStretchOpacity([path[i], path[i + 1]], 'show');
+            changeStationOpacity(path[i + 1], 'show');
+            yield delay(1000);
+        }
+    });
 }
 function changeMapOpacity(action) {
     const opacityStyleClasses = {
@@ -138,4 +172,9 @@ function changeLineStretchOpacity(stretch, action) {
     const lineStretch = (_a = document.getElementById(`metro-stretch-${stretch[0]}-${stretch[1]}`)) !== null && _a !== void 0 ? _a : document.getElementById(`metro-stretch-${stretch[1]}-${stretch[0]}`);
     lineStretch.classList.add(opacityStyleClasses[action].toAdd);
     lineStretch.classList.remove(opacityStyleClasses[action].toRemove);
+}
+function delay(milliseconds) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, milliseconds);
+    });
 }
--- busca-heuristica/js/main.ts
+++ busca-heuristica/js/main.ts
@@ -15,35 +15,46 @@ import {
     let selectedDestinationStation: StationIdentifier = null;
     
     const updateCalculateRouteButtonAvailability = () => {
-        if (selectedDepartureStation != null && selectedDestinationStation != null) {
+        if (selectedDepartureStation != null && selectedDestinationStation != null && selectedDepartureStation !== selectedDestinationStation) {
             calculateRouteButton.disabled = false;
         } else {
             calculateRouteButton.disabled = true;
         }
     };
 
+    const clearInterface = () => {
+        changeMapOpacity('show');
+        document.getElementById('route-time-indicator').classList.add('hidden-element');
+    };
+
     departureStationSelectionDropdown.addEventListener('sl-change', (event) => {
         selectedDepartureStation = event.target.value;
         updateCalculateRouteButtonAvailability();
+        clearInterface();
     });
 
     destinationStationSelectionDropdown.addEventListener('sl-change', (event) => {
         selectedDestinationStation = event.target.value;
         updateCalculateRouteButtonAvailability();
+        clearInterface();
     });
 
     calculateRouteButton.addEventListener('click', () => {
         const aStarResult = runAStarAlgorithm(selectedDepartureStation, selectedDestinationStation);
+        const path = processAStarPath(aStarResult, selectedDepartureStation, selectedDestinationStation);
+        const tripTime = aStarResult.get(selectedDestinationStation).realTimeFromDepartureToNodeInMinutes;
+
+        updateInterfaceWithRoute(path, tripTime);
     });
 })()
 
-function runAStarAlgorithm(departureStation: StationIdentifier, destinationStation: StationIdentifier) {
-    type NodeData = {
-        station: StationIdentifier,
-        realTimeFromDepartureToNodeInMinutes: number,
-        estimatedTimeFromNodeToDestinationInMinutes: number,
-    }
+type NodeData = {
+    station: StationIdentifier,
+    realTimeFromDepartureToNodeInMinutes: number,
+    estimatedTimeFromNodeToDestinationInMinutes: number,
+}
 
+function runAStarAlgorithm(departureStation: StationIdentifier, destinationStation: StationIdentifier) {
     const estimatedTotalTimeGetter = (nodeData: NodeData) => {
         return nodeData.realTimeFromDepartureToNodeInMinutes + nodeData.estimatedTimeFromNodeToDestinationInMinutes;
     };
@@ -55,38 +66,48 @@ function runAStarAlgorithm(departureStation: StationIdentifier, destinationStati
         estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(departureStation, destinationStation)
     })
     const closedList = new Map<StationIdentifier, NodeData>();
-
-    let isFinalDestination = false;
-
-    while (openList.size > 0 && !isFinalDestination) {
-        const currentNode = openList.removeSmallest();
+    
+    while (openList.size > 0) {
+        let currentNode = openList.removeSmallest();
 
         if (currentNode.station === destinationStation) {
-            isFinalDestination = true;
-        } else {
-            const adjacentStations = STATIONS[currentNode.station].realDistances;
-            for (const [adjacentStation, realDistance] of Object.entries(adjacentStations) as [StationIdentifier, number][]) {
-                if (realDistance != null) {
-                    const realTimeFromDepartureToNodeInMinutes =
-                        currentNode.realTimeFromDepartureToNodeInMinutes +
-                        getRealTripTimeInMinutes(currentNode.station, adjacentStation, null) // FIXME
-                    if (!closedList.has(adjacentStation) || realTimeFromDepartureToNodeInMinutes < closedList.get(adjacentStation).realTimeFromDepartureToNodeInMinutes) {
-                        closedList.set(currentNode.station, {
-                            station: adjacentStation,
-                            realTimeFromDepartureToNodeInMinutes,
-                            estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation)
-                        })
+            return closedList;
+        }
+
+        const adjacentStations = STATIONS[currentNode.station].realDistances;
+        for (const [adjacentStation, realDistance] of Object.entries(adjacentStations) as [StationIdentifier, number][]) {
+            if (realDistance != null) {
+                const realTimeFromDepartureToNodeInMinutes =
+                    currentNode.realTimeFromDepartureToNodeInMinutes +
+                    getRealTripTimeInMinutes(currentNode.station, adjacentStation)
+
+                if (!closedList.has(adjacentStation) || realTimeFromDepartureToNodeInMinutes < closedList.get(adjacentStation).realTimeFromDepartureToNodeInMinutes) {
+                    closedList.set(adjacentStation, {
+                        station: currentNode.station,
+                        realTimeFromDepartureToNodeInMinutes,
+                        estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation),
+                    });
+                    if (!openList.find(node => node?.station === adjacentStation)) {
                         openList.insert({
                             station: adjacentStation,
                             realTimeFromDepartureToNodeInMinutes,
-                            estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation)
+                            estimatedTimeFromNodeToDestinationInMinutes: getEstimatedTripTimeInMinutes(adjacentStation, destinationStation),
                         })
                     }
                 }
             }
         }
     }
+}
 
+function processAStarPath(aStarResult: Map<StationIdentifier, NodeData>, departureStation: StationIdentifier, destinationStation: StationIdentifier): StationIdentifier[] {
+    const path: StationIdentifier[] = [destinationStation];
+
+    while (path[0] !== departureStation) {
+        path.unshift(aStarResult.get(path[0]).station);
+    }
+
+    return path;
 }
 
 function calculateTrainTimeInMinutes(distance: number): number {
@@ -97,12 +118,23 @@ function getEstimatedTripTimeInMinutes(departureStation: StationIdentifier, dest
     return calculateTrainTimeInMinutes(STATIONS[departureStation].estimatedDistances[destinationStation]);
 }
 
-function getRealTripTimeInMinutes(departureStation: StationIdentifier, destinationStation: StationIdentifier, currentLine: MetroLine): number {
-    let tripTime = calculateTrainTimeInMinutes(STATIONS[departureStation].realDistances[destinationStation]);
-    if (!STATIONS[destinationStation].lines.includes(currentLine)) {
-        tripTime += LINE_EXCHANGE_TIME_IN_MINUTES;
+function getRealTripTimeInMinutes(departureStation: StationIdentifier, destinationStation: StationIdentifier): number {
+    return calculateTrainTimeInMinutes(STATIONS[departureStation].realDistances[destinationStation]);
+}
+
+async function updateInterfaceWithRoute(path: StationIdentifier[], tripTime: number) {
+    const routeTimeIndicator = document.getElementById('route-time-indicator');
+    routeTimeIndicator.innerHTML = `Tempo da rota: <strong>${tripTime.toLocaleString('pt-BR', {maximumFractionDigits: 2})} minutos</strong>`;
+    routeTimeIndicator.classList.remove('hidden-element');
+
+    changeMapOpacity('fade');
+
+    for (let i = 0; i + 1 < path.length; i++) {
+        changeStationOpacity(path[i], 'show');
+        changeLineStretchOpacity([path[i], path[i + 1]], 'show');
+        changeStationOpacity(path[i + 1], 'show');
+        await delay(1000);
     }
-    return tripTime;
 }
 
 function changeMapOpacity(action: 'fade' | 'show') {
@@ -168,4 +200,10 @@ function changeLineStretchOpacity(stretch: [StationIdentifier, StationIdentifier
 
     lineStretch.classList.add(opacityStyleClasses[action].toAdd);
     lineStretch.classList.remove(opacityStyleClasses[action].toRemove);
+}
+
+function delay(milliseconds: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(resolve, milliseconds)
+    });
 }
